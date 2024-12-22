22b528371b0ff74ae67d7bc8c3f09f9ab03e97c3
gotocoffee <gotocoffee1@gmail.com>
2024-12-22 23:26:58 +0100

    round corners of zellij & use sta

--- home/shell/zellij.nix
+++ home/shell/zellij.nix
@@ -3,6 +3,14 @@
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      ui = {
+        pane_frames = {
+          rounded_corners = true;
+          hide_session_name = true;
+        };
+      };
+    };
   };
 }
 
