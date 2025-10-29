d2acd7c9f482e0c784d386e06b6fd94b4ee8cf44
gotocoffee <gotocoffee1@gmail.com>
2025-10-29 22:43:27 +0100

    better rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,5 @@ script_dir=$(realpath $(dirname $0))
 host=${1:-vm}
 specialisation=${2:+--specialisation $2}
 
-sudo nixos-rebuild --impure --flake .#$host switch $specialisation
+nixos-rebuild --use-remote-sudo --impure --flake .#$host switch $specialisation
 
