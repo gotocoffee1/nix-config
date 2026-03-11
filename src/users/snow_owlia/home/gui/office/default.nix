6f6e8b706a51a89006d5bc733ec17b6467c3a142
gotocoffee <gotocoffee1@gmail.com>
2026-03-11 18:29:24 +0100

    small fix

--- src/users/snow_owlia/home/gui/office/default.nix
+++ src/users/snow_owlia/home/gui/office/default.nix
@@ -3,9 +3,11 @@
   imports = [
     ./firefox.nix
     ./keepassxc.nix
-    ./thunderbird.nix
+    #./thunderbird.nix
   ];
   home.packages = with pkgs; [
     libreoffice
   ];
+  # https://github.com/nix-community/home-manager/issues/6188
+  gtk.gtk2.force = true;
 }
